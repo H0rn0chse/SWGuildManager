267222ff99ca23ebbe4580bd67dd30e7b167f30c
H0rn0chse <h0rn0chse@gmx.de>
2021-12-13 17:57:27 +0100

    Bumped @h0rn0chse/socket-server to 1.0.3

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 import { startServer, registerXhrHandler } from "@h0rn0chse/socket-server";
 
 import { DatabaseManager } from "./server/DatabaseManager.js";
-import { root } from "./server/globals.js"
 
 startServer({
     publicPaths: [
         ["/client", "/"],
         ["/shared", "/shared"]
-    ],
-    root
+    ]
 });
 
 DatabaseManager.connect()
