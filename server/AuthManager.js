e3d81cd5018beb81e86cdd85ce4accbefeec418b
H0rn0chse <h0rn0chse@gmx.de>
2022-01-16 18:56:44 +0100

    Implemented basic auth features

--- server/AuthManager.js
+++ server/AuthManager.js
@@ -1,20 +1,51 @@
 import { registerXhrHandler } from "@h0rn0chse/socket-server";
+import { DatabaseManager } from "./DatabaseManager.js";
 
 class _AuthManager {
     init () {
         registerXhrHandler("get", "/authorization", this.handleAuthorization, this);
     }
 
-    handleAuthorization (req, res, token) {
-        const response = [];
+    async handleAuthorization (req, res, token) {
+        let response = [];
 
-        if (token) {
-            response.push("PROFILE_EDIT");
-            response.push("MEMBER_LIST_EDIT");
+        const member = await DatabaseManager.getMemberByToken(token);
+        if (!member) {
+            res.status(403);
+        } else {
+            response = member.permissions;
+            res.status(200);
+            res.json(response);
         }
 
-        res.json(response);
         res.end();
+        return true;
+    }
+
+    async hasPermission (token, permission) {
+        const member = await DatabaseManager.getMemberByToken(token);
+
+        if (!member) {
+            console.log(`Permission "${permission}" denied for "${token}" reason: "User not found"`);
+            return false;
+        }
+
+        const result = member.permissions.includes(permission);
+        if (result) {
+            console.log(`Permission "${permission}" granted for "${token}"`);
+        } else {
+            console.log(`Permission "${permission}" denied for "${token}" reason: "Permission not found`);
+        }
+
+        return result;
+    }
+
+    getPermissions () {
+        return [
+            "PROFILE_EDIT",
+            "MEMBER_LIST_EDIT",
+            "ADMIN_EDIT"
+        ];
     }
 }
 
--- server/DatabaseManager.js
+++ server/DatabaseManager.js
@@ -18,7 +18,7 @@ class _DatabaseManager {
         mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
             .then(() => {
                 globalThis.console.log("Database connected");
-                //this.loadModel();
+                this.loadModel();
                 this.deferred.resolve();
             })
             .catch((err) => {
@@ -29,13 +29,88 @@ class _DatabaseManager {
     }
 
     loadModel () {
-        /*const Highscore = new Schema({
-            placement: Number,
+        const MemberSchema = new Schema({
             name: String,
-            score: Number,
-            date: Number,
+            token: String,
+            permissions: [
+                {
+                    id: String
+                }
+            ],
         });
-        this.highscore = mongoose.model("Highscore", Highscore);*/
+        this.memberModel = mongoose.model("Member", MemberSchema);
+    }
+
+    async getMembers () {
+        const memberList = await this.memberModel.find();
+        const memberListCopy = [];
+        memberList.forEach((member) => {
+            const memberCopy = member.toObject();
+            // flatten permissions
+            memberCopy.permissions = memberCopy.permissions.map((object) => {
+                return object.id;
+            });
+            memberListCopy.push(memberCopy);
+        });
+        return memberListCopy;
+    }
+
+    async getMemberByToken (token) {
+        const member = await this.memberModel.findOne({ token });
+        if (!member) {
+            return null;
+        }
+        const memberCopy = member.toObject();
+
+        // flatten permissions
+        memberCopy.permissions = memberCopy.permissions.map((object) => {
+            return object.id;
+        });
+        return memberCopy;
+    }
+
+    async addMember (name, token, permissions) {
+        // expand permissions
+        const perm = permissions.map((name) => {
+            return {
+                id: name
+            };
+        });
+        const member = await this.memberModel.create({ name, token, perm });
+        return member;
+    }
+
+    async updateMemberProps (id, props) {
+        const member = await this.memberModel.findById(id);
+        if (!member) {
+            return null;
+        }
+
+        Object.keys(props).forEach((key) => {
+            let value = props[key];
+            if (key === "permissions") {
+                // expand permissions
+                value = props[key].map((name) => {
+                    return {
+                        id: name
+                    };
+                });
+            }
+
+            member[key] = value;
+        });
+
+        return member.save();
+    }
+
+    async deleteMember (id) {
+        const result = await this.memberModel.deleteOne({ _id: id });
+        return result.deletedCount === 1;
+    }
+
+    async deleteMemberByToken (token) {
+        const result = await this.memberModel.deleteOne({ token });
+        return result.deletedCount === 1;
     }
 }
 
